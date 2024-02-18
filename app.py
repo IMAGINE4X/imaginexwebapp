97d38a6a3e6c492d3d9fd99fecd46a51489c6760
berasoumya2002 <berasoumya2002@gmail.com>
2024-02-18 18:52:17 +0000

    Third Commit : Added Some Basic pages and Designs

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return render_template('base.html')
+    return render_template('home.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/about')
+def about():
+    return render_template('aboutus.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contactus.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
